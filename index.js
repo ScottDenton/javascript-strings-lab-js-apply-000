3dff78fefc9b6319e88a30db9257633d2d1af60c
scottyd_14 <scottyd_14@hotmail.com>
2018-09-21 20:09:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+function greeting(){
+  return ("Hello, everybody!")
+}
+
+function greetSpecialGuest(name){
+  return (`Hello, ${name}!`)
+}
+
+function conversation(topic){
+  return (`Lets talk about ${topic}.`)
+}
